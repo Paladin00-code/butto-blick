ef728e3fc8aa84cf5f6a4899bf0e61b1dfa09fb2
harshperson <harshperson@gmail.com>
2020-04-15 18:37:55 +0300

    add rotate in js

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 function parallax(e) {
-    let layer = document.querySelector('.layer-bg');
+    let layer = this.querySelector('.layer-bg');
     layer.style.transform = `translate(${e.clientX/50}px, ${e.clientY/50}px)`;
+    
+    let h1 = this.querySelector('h1');
+    h1.style.transform = `rotateZ(${e.clientX-100/150}deg)`;
+    
+    let button = this.querySelector('.button');
+    button.style.transform = `translate(${e.clientX/-150}px, ${e.clientY/-150}px)`;
 };
 
 document.addEventListener('mousemove', parallax);
